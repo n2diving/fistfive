e536fc0ebe27e3f21e21239bbeb17e25fa18b3dd
Cha2d <chadd.chambers@gmail.com>
2014-08-24 14:07:59 -0700

    Created students/instructors controllers, sample tests, and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,62 @@
 class UsersController < ApplicationController
 
-def new
-  
-end
+  def new
+    @user = User.new
+    @is_signup = true
+    if current_user
+      redirect to users_path
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  # def show
+  #   @user = User.where(params[:id]).first
+  # end
+
+  # def edit
+  #   @user = User.where(params[:id]).first
+  # end
+
+  # def update
+  #   @user = User.where(params[:id]).first
+  #   if @user.update_attributes(user_params)
+  #     redirect_to users_path
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+
+  def create
+    if params[:form_tag] == "Student"
+      @user = Student.new(user_params)
+    else
+      @user = Instructor.new(user_params)
+    end
+    if @user.save
+      session[:user_id] = @user.id.to_s
+      if @user.type == "Student"
+        redirect_to users_path
+      else
+        redirect_to instructors_path
+      end
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to users_path    
+  # end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :type, :email, :password, :password_confirmation)
+  end
 
 end
+
