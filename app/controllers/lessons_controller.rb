4c23fc193e63a730964e80ad7a2f03b26460acae
hamitron <hqkarl@gmail.com>
2014-08-25 12:32:43 -0700

    added gon gem for passing the data around easier.  building out lessons controller and views.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,30 @@
 class LessonsController < ApplicationController
+	def index
+
+  # some controller logic
+  @lessons = Lesson.all
+
+  gon.jbuilder
+  # some controller logic
+
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
