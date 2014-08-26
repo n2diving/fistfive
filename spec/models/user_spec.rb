dd62a31af007c3660638ddcfe3e4c50de0ad4110
hamitron <hqkarl@gmail.com>
2014-08-26 15:17:55 -0700

    question model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,21 @@
+require 'rails_helper'
 require "spec_helper"
-require "users.rb"
+# require "users.rb"
 
-Rspec.describe "User" do
+Rspec.describe User do
   it "should have a type" do
-    User.new.should be_an_instance_of(User)
+    User.new.should be_an_instance_of(:user)
     # (:type => "Student")
     # u.type == "Student"
   end
 
 
-  it "should have a valid name"
-  it "is invalid without unique email"
-  it "should have valid password"
+  # it "should have a valid name"
+  # it "is invalid without unique email"
+  # it "should have valid password"
 end
 
-# require 'rails_helper'
+
 
 # RSpec.describe User, :type => :model do
 #   pending "add some examples to (or delete) #{__FILE__}"
