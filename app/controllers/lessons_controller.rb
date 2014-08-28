9cfdb314adaab2be9ac2ec2541c6a72e7b4e5a1a
hamitron <hqkarl@gmail.com>
2014-08-27 19:15:47 -0700

    further additions to lesson CRUD.  displays lists of lessons, with linked instructor.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -22,22 +22,34 @@ class LessonsController < ApplicationController
 	end
 
 	def create
-		@lesson = Lesson.new
+		@lesson = Lesson.new lesson_params
 		if @lesson.save
-			redirect_to lesson_path
+			redirect_to lessons_path
 		else 
 			render 'new'
 		end
 	end
 
 	def edit
+		@lesson = Lesson.find(params[:id])
+		if @lesson.update(lesson_params)
+			redirect_to lessons_path
+		else
+			render 'edit'
+		end
 	end
 
 	def update
 	end
 
 	def destroy
+		@lesson = Lesson.find(params[:id])
+		@lesson.destroy
+		redirect_to lessons_path
 	end
-
+protected
+	def lesson_params
+		params.require(:lesson).permit(:name, :instructor_id, :is_active)
+	end
 
 end
