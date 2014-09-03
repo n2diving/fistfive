3940cebcbb4e9a8ecf060f7eb4bb08ee6473d0e1
hamitron <hqkarl@gmail.com>
2014-09-02 20:44:27 -0700

    small changes

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,10 +8,11 @@ class Lesson < ActiveRecord::Base
   has_many :student_lessons
   has_many :students, through: :student_lessons
 
+# filtering by each student's last rating
+# filtering it down per student
 
 
-
- def class_avg
+def class_avg
 
   # this averages the total ratings for the class
   avg = ratings.average :value
@@ -24,7 +25,7 @@ def value_counts
 
   # returns a hash of the different values and the amount of ratings that associate with that value
   ratings.group(:value).count 
-  
+
 end
 
   
