ba658447f927ecbcafe1a6ba0582068692f3bee3
Cha2d <chadd.chambers@gmail.com>
2014-09-07 14:30:52 -0700

    Student/Instructor login/signup is now working properly

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < UsersController
+  
   def index
     @student = current_user
-    @lessons = Lesson.where(params[:id])
+    @lessons = Lesson.all
   end
 
   def new
-    @user = User.new
+    @user = Student.new
     @is_signup = true
     if current_user
       redirect_to students_path
@@ -13,8 +14,7 @@ class StudentsController < UsersController
   end
 
   def create
-    @user = User.new(user_params)
-
+    @user = Student.new(user_params)
     if @user.save
       session[:user_id] = @user.id.to_s
       redirect_to students_path
@@ -23,31 +23,31 @@ class StudentsController < UsersController
     end
   end
 
-  def edit
-    @user = User.where(params[:id]).first
-  end
+  # def edit
+  #   @user = User.where(params[:id]).first
+  # end
 
-  def show
-    @user = User.where(params[:id]).first
-  end
+  # def show
+  #   @user = User.where(params[:id]).first
+  # end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path    
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to users_path    
+  # end
 
-  def update
-    @user = User.where(params[:id]).first
-    if @user.update_attributes(user_params)
-      redirect_to users_path
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   @user = User.where(params[:id]).first
+  #   if @user.update_attributes(user_params)
+  #     redirect_to users_path
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   private
   def user_params
-    params.require(:user).permit(:name, :type, :email, :password, :password_confirmation)
+    params.require(:student).permit(:name, :type, :email, :password, :password_confirmation)
   end
 end
